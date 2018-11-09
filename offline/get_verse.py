e33ea526c1f7a3b9558eca94213cc8cfd16ed781
doantientai <doantientai@gmail.com>
2018-11-09 08:05:42 +0100

    VIET.json done

--- offline/get_verse.py
+++ offline/get_verse.py
@@ -109,46 +109,54 @@ def verse_lookup(addr_str, version="VIET"):
 
 
 def multi_verse_lookup(addr_str, version):
-    ### check if this address contains multiple sentences
-    addr_chapter = addr_str[:addr_str.index(":")]
-    addr_sentences = addr_str[addr_str.index(":") + 1:]
+    try:
+        ### check if this address contains multiple sentences
+        addr_chapter = addr_str[:addr_str.index(":")]
+        addr_sentences = addr_str[addr_str.index(":") + 1:]
 
-    ### Reject if address contains 2 colons
-    if ":" in addr_sentences:
-        print("Invalid address:", addr_str)
-        exit()
+        ### Reject if address contains 2 colons
+        if ":" in addr_sentences:
+            print("Invalid address:", addr_str)
+            # exit()
+            return None, None
 
-    if "-" in addr_sentences:
-        sentence_begin = addr_sentences[:addr_sentences.index("-")]
-        sentence_end = addr_sentences[addr_sentences.index("-") + 1:]
-        # print(addr_sentences)
-        # print(sentence_begin)
-        # print(sentence_end)
+        if "-" in addr_sentences:
+            sentence_begin = addr_sentences[:addr_sentences.index("-")]
+            sentence_end = addr_sentences[addr_sentences.index("-") + 1:]
+            # print(addr_sentences)
+            # print(sentence_begin)
+            # print(sentence_end)
 
-        ### look up each sentence
-        verse_content_sum = ""
-        verse_address_sum = ""
-        got_book_chapter = False
+            ### look up each sentence
+            verse_content_sum = ""
+            verse_address_sum = ""
+            got_book_chapter = False
 
-        if int(sentence_begin) < int(sentence_end):
-            for number in range(int(sentence_begin), int(sentence_end) + 1):
-                verse_content, addr_vi = verse_lookup(addr_chapter + ":" + str(number), version=version)
-                verse_content_sum = verse_content_sum + " " + verse_content
+            if (int(sentence_begin) < int(sentence_end)) and (int(sentence_end) - int(sentence_begin) <= 5):
+                for number in range(int(sentence_begin), int(sentence_end) + 1):
+                    verse_content, addr_vi = verse_lookup(addr_chapter + ":" + str(number), version=version)
+                    verse_content_sum = verse_content_sum + " " + verse_content
 
-                if not got_book_chapter:
-                    verse_address_sum = addr_vi[:addr_vi.index(":")]
+                    if not got_book_chapter:
+                        verse_address_sum = addr_vi[:addr_vi.index(":")]
+            else:
+                print("Invalid address", addr_str)
+                # exit()
+                return None, None
+
+            ### take the address in target language
+            verse_address_sum = verse_address_sum + ":" + addr_sentences
+
         else:
-            print("Invalid address", addr_str)
-            exit()
+            verse_content_sum, verse_address_sum = verse_lookup(addr_str, version=version)
 
-        ### take the address in target language
-        verse_address_sum = verse_address_sum + ":" + addr_sentences
-
-    else:
-        verse_content_sum, verse_address_sum = verse_lookup(addr_str, version=version)
-
-    # print("verse_content_sum", verse_content_sum)
-    # print("verse_address_sum", verse_address_sum)
+        # print("verse_content_sum", verse_content_sum)
+        # print("verse_address_sum", verse_address_sum)
+    except Exception as e:
+        print(e)
+        verse_content_sum = None
+        verse_address_sum = None
+        pass
     return verse_content_sum, verse_address_sum
 
 
